98bd1142fb037ad8d006d0128393bdee71ddd4aa
Vitor Migoto <vitormigoto@gmail.com>
2023-12-23 15:18:43 -0300

    feat: creating variables with data client

---
+++ src/Desafio.java
@@ -0,0 +1,24 @@
+public class Desafio {
+    public static void main(String[] args) {
+        /*
+        Objetivo: Criar um controle de conta virtual
+        Dados do cliente virtual:
+            Nome:               Vitor Migoto
+            Tipo de Conta:      Corrente
+            Saldo Inicial:      R$ 100,00
+        */
+
+        String nomeCliente = "Vitor Migoto";
+        String tipoConta = "Corrente";
+        double saldoConta = 100;
+
+        // Exibindo dados do cliente
+        System.out.println("******************************************************");
+        System.out.println("Nome do Cliente:        " + nomeCliente);
+        System.out.println("Tipo de Conta:          " + tipoConta);
+        System.out.println("Saldo Inicial:          R$ " + saldoConta);
+        System.out.println("******************************************************");
+
+
+    }
+}
